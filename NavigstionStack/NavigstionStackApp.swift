c661fbd569bbeee2e03b38d07862f783937b3a80
Dagmar <beckeldagmar@gmail.com>
2023-07-14 09:54:02 -0500

    Initial Commit

---
+++ NavigstionStack/NavigstionStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigstionStackApp.swift
+//  NavigstionStack
+//
+//  Created by GoldenCalfCompanyMacPro2 on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigstionStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
